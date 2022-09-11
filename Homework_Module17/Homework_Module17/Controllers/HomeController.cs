bbf7c297602bae15dd1f407879a8182ec91c5c91
Konstantin.Borovik <konstantin.borovik@fortegrp.com>
2022-09-11 12:36:19 +0300

    - init commit

---
+++ Homework_Module17/Homework_Module17/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Homework_Module17.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
