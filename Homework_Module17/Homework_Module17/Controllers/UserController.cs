ca4de23550a8b7b77fd900a74d774f80b924d1bb
IliukhaDobrik <bogdobrinskiy@gmail.com>
2022-09-11 22:12:19 +0300

    COMPLETED controller UserController

---
+++ Homework_Module17/Homework_Module17/Controllers/UserController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Homework_Module17.Services;
+using Homework_Module17.Models;
+
+namespace Homework_Module17.Controllers
+{
+    public class UserController : Controller
+    {
+        //public static readonly UserService userService = new UserService();
+        public IActionResult Index()
+        {
+            return View(UserService.GetUsers());
+        }
+
+        [HttpGet]
+        public IActionResult AddUser()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddUser(UserViewModel user)
+        {
+            UserService.AddUser(user);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult GetUser(int index = 0)
+        {
+            return View(UserService.GetUser(index));
+        }
+
+        [HttpGet]
+        public IActionResult DeleteUser()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult DeleteUser(int index)
+        {
+            UserService.DeleteUser(index);
+            return RedirectToAction("Index");
+        }
+    }
+}
--- Homework_Module17/Homework_Module17/Program.cs
+++ Homework_Module17/Homework_Module17/Program.cs
@@ -9,6 +9,6 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.MapControllerRoute(name: "default",
-                       pattern: "{controller=Home}/{action=Index}/{id?}");
+                       pattern: "{controller=User}/{action=Index}/{id?}");
 
 app.Run();
