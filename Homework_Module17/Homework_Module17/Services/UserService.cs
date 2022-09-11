2380311dd5f9389f005a5875562c898946c185d2
IliukhaDobrik <bogdobrinskiy@gmail.com>
2022-09-11 19:15:17 +0300

    CREATED static class UserService

---
+++ Homework_Module17/Homework_Module17/Services/UserService.cs
@@ -0,0 +1,29 @@
+﻿using Homework_Module17.Models;
+
+namespace Homework_Module17.Services
+{
+    public static class UserService
+    {
+        private static List<UserViewModel> _users = new List<UserViewModel>();
+
+        public static List<UserViewModel> GetUsers()
+        {
+            return _users;
+        }
+
+        public static UserViewModel GetUser(int index)
+        {
+            return _users[index];
+        }
+
+        public static void AddUser(UserViewModel user)
+        {
+            _users.Add(user);
+        }
+
+        public static void DeleteUser(int index)
+        {
+            _users.RemoveAt(index);
+        }
+    }
+}
