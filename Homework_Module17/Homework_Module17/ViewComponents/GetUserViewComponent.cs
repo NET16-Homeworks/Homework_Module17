aa77c35f3239c40367b238e089a9037cbaa02c9d
IliukhaDobrik <bogdobrinskiy@gmail.com>
2022-09-11 21:46:14 +0300

    CREATED GetUserViewComponent

---
+++ Homework_Module17/Homework_Module17/ViewComponents/GetUserViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Homework_Module17.Services;
+
+namespace Homework_Module17.ViewComponents
+{
+    public class GetUserViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View("GetUser", UserService.GetUser(0));
+        }
+    }
+}
