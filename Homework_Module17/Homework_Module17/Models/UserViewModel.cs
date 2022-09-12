7bbb28491a886787589676aa36f41536ca8214d5
Artyom Zhukovsky <Artyom.Zhukovsky@dip.co.uk>
2022-09-12 15:35:37 +0300

    add homework #15

---
+++ Homework_Module17/Homework_Module17/Models/UserViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Homework_Module17.Models
+{
+    public class UserViewModel
+    {
+        public string Name { get; set; }
+        public UserSex Sex { get; set; }
+        public string Country { get; set; }
+        public bool IsMarried { get; set; }
+    }
+}
+
+public enum UserSex
+{
+    Male, Female
+}
---
+++ Homework_Module17/Homework_Module17/Services/UserService .cs
@@ -0,0 +1,36 @@
+﻿using Homework_Module17.Models;
+
+namespace Homework_Module17.Services
+{
+    public static class UserService
+    {
+        static List<UserViewModel> _users = new()
+        {
+            new UserViewModel() { Name = "User1", Country = "Беларусь", IsMarried = true, Sex = UserSex.Male },
+            new UserViewModel() { Name = "User2", Country = "США", IsMarried = false, Sex = UserSex.Female },
+            new UserViewModel() { Name = "User3", Country = "Украина", IsMarried = true, Sex = UserSex.Male },
+            new UserViewModel() { Name = "User4", Country = "Бельгия", IsMarried = false, Sex = UserSex.Female },
+            new UserViewModel() { Name = "User5", Country = "Норвегия", IsMarried = true, Sex = UserSex.Male },
+        };
+
+        public static List<UserViewModel> GetUsers()
+        {
+            return _users;
+        }
+
+        public static UserViewModel GetUser(int index)
+        {
+            return _users[index];
+        }
+
+        public static void AddUser(UserViewModel user)
+        {
+            _users.Add(user);
+        }
+
+        public static void DeleteUser(int index)
+        {
+            _users.RemoveAt(index);
+        }
+    }
+}
