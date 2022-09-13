42510881f6a14fdbf47709ea95374151a1fa0cb7
danen <danenkov.dmitriy@list.ru>
2022-09-13 21:15:01 +0300

    F

---
+++ Homework_Module17/Homework_Module17/Services/UserService.cs
@@ -0,0 +1,38 @@
+﻿using Homework_Module17.Models;
+
+namespace Homework_Module17.Services
+{
+    public class UserService
+    {
+        static List<UserViewModel> _users = new()
+        {
+            new UserViewModel() { Name = "Dmitriy", Country = "Belarus", IsMarried = true, Sex = UserSex.Male },
+            new UserViewModel() { Name = "Dasha", Country = "Poland", IsMarried = false, Sex = UserSex.Female },
+            new UserViewModel() { Name = "Vasiliy", Country = "Ukraine", IsMarried = true, Sex = UserSex.Male },
+        };
+        /*GetUsers() - возвращает всех юзеров
+GetUser(index) - возвращает юзера по индексу
+AddUser(user) - добавляет юзера в массив данных
+DeleteUser(index) - удаляет юзера по индексу
+*/
+        public static List<UserViewModel> GetUsers()
+        {
+            return _users;
+        }
+
+        public static UserViewModel GetUser(int index)
+        {
+            return _users[index];
+        }
+
+        public static void AddUser(UserViewModel user)
+        {
+            _users.Add(user);
+        }
+
+        public static void DeleteUser(int index)
+        {
+            _users.RemoveAt(index);
+        }
+    }
+}
---
+++ Homework_Module17/Homework_Module17/ViewComponents/GetUserViewComponent/GetUserViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using Homework_Module17.Services;
+using Microsoft.AspNetCore.Mvc;
+
+    namespace Homework_Module17.ViewComponents
+    {
+        public class GetUserViewComponent : ViewComponent
+        {
+            public IViewComponentResult Invoke(int index)
+            {
+                return View("GetUser", UserService.GetUser(index));
+            }
+        }
+   }
+
