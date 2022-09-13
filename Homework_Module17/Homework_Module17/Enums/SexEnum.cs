c3f05232455ea30ea97fc383b4cb5ade19c994bb
Ruslan Soroko <rus.soroko@gmail.com>
2022-09-13 20:01:56 +0300

    Homework Module 17 completed

---
+++ Homework_Module17/Homework_Module17/Enums/SexEnum.cs
@@ -0,0 +1,8 @@
+﻿namespace Homework_Module17.Enums
+{
+    public enum SexEnum
+    {
+        Man = 1,
+        Woman
+    }
+}
---
+++ Homework_Module17/Homework_Module17/Services/UserService.cs
@@ -0,0 +1,55 @@
+﻿using Homework_Module17.ViewModels;
+
+namespace Homework_Module17.Services
+{
+    public class UserService
+    {
+        public static List<UserViewModel> _users = new List<UserViewModel>
+        {
+            new UserViewModel
+            {
+                Name = "Pasha",
+                Sex = Enums.SexEnum.Man,
+                Country = "Belarus",
+                IsMarried = false
+            },
+            new UserViewModel
+            {
+                Name = "Alexandra",
+                Sex = Enums.SexEnum.Woman,
+                Country = "Ukrain",
+                IsMarried = true
+            },
+            new UserViewModel
+            {
+                Name = "Amelia",
+                Sex = Enums.SexEnum.Woman,
+                Country = "Poland",
+                IsMarried = false
+            }
+        };
+
+        public List<UserViewModel> GetUsers()
+        {
+            return _users;
+        }
+
+        public UserViewModel GetUser(int index)
+        {
+            var user = _users[index];
+            return user;
+        }
+
+        public void AddUser(UserViewModel user)
+        {
+            _users.Add(user);
+        }
+
+        public void DeleteUser(int index)
+        {
+            _users.RemoveAt(index);
+        }
+
+
+    }
+}
